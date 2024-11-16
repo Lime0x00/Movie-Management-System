86f7bda057243fece1b05c74b11a4058aa3aff9f
lime0x001 <mohammada4raf@gmail.com>
2024-11-16 11:44:59 +0200

    Update Main class to support future GUI/CLI integration
    - Added placeholder for switching between CLI and GUI modes using command-line arguments.
    - Retained the CLI functionality with a `Manager` instance initialized for command-line use.
    - Included a comment to enable seamless GUI integration in the future once implemented.
    - Improved maintainability by preparing the code structure for dual execution modes (GUI/CLI).

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,18 @@
-import CLI.Manager;
 import java.util.Scanner;
 public class Main {
 
     public static void main (String[] args) {
 
         Scanner scanObj = new Scanner(System.in);
-        Manager cli = new Manager(scanObj);
+        CLI.Manager cli = new CLI.Manager(scanObj);
         cli.start(scanObj);
-
+ //! Remove the comment after implementing GUI   (to make same exe file run GUI/CLI)
 //        if (args.length > 1 && args[1].equals("--cli")) {
 //            Scanner scanObj = new Scanner(System.in);
 //            Manager cli = new Manager(scanObj);
 //            cli.start(scanObj);
 //        } else {
-//            //USE MANAGER IN GUI
+//            GUI.Manager gui = new GUI.Manager();
 //        }
-//
-
     }
 }
