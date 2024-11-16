92d716cef0c492a4023814b0ecbcda8ca255c057
lime0x001 <mohammada4raf@gmail.com>
2024-11-16 03:00:00 +0200

    Add movie deletion feature and update main menu
    - Added "Delete Movie" option to the main menu for administrators.
    - Implemented functionality to delete a movie, including user confirmation steps.
    - Improved user feedback with success and error messages during movie deletion.
    - Updated `generateReports` and `bookMovie` methods for consistency and better readability.
    - Enhanced error handling for invalid options in the main menu.
    - Refactored `confirmMsg` to improve movie and seat booking confirmation flow.

--- src/CLI/Menu.java
+++ src/CLI/Menu.java
@@ -1,25 +1,25 @@
 package CLI;
 
 import Book.Customer;
-import Book.Order;
 import Book.Report;
-import Hall.Seat;
 import Movie.Movie;
+import Movie.MovieLibrary;
 import Movie.ScreenTime;
-import java.util.*;
+
+import java.util.Scanner;
 
 public class Menu {
     static final String[] mainMenu = {
-            "Book Movie.Movie",
+            "Book Movie",
             "Generate Reports",
             "List Receipts",
             "Add Movie",
+            "Delete Movie",
             "Log-out",
     };
 
     public static void mainMenu(Scanner scanObj, Customer customer) {
         while (true) {
-            System.out.flush();
             for (int i = 0; i < Menu.mainMenu.length; i++) {
                 System.out.println((i + 1) + ": " + Menu.mainMenu[i]);
             }
@@ -37,6 +37,7 @@ public class Menu {
                 case 2 -> generateReports(scanObj);
                 case 3 -> listReceipts(scanObj, customer);
                 case 4 -> addMovie(scanObj);
+                case 6 -> deleteMovie(scanObj);
                 case 5 -> {
                     return;
                 }
@@ -47,8 +48,6 @@ public class Menu {
     }
 
     public static void bookMovie (Scanner scanObj, Customer customer) {
-        System.out.flush();
-
         var movie = Select.movie(scanObj);
 
         ScreenTime screenTime = Select.screenTime(scanObj, movie);
@@ -56,14 +55,14 @@ public class Menu {
 
         var selectedSeats = Select.seats(scanObj, hall);
 
-        if (confirmMsg(scanObj, movie, selectedSeats, screenTime)) {
+        if (Check.confirmMsg(scanObj, movie, selectedSeats, screenTime)) {
             var receipt =  customer.bookMovie(screenTime, movie, selectedSeats);
             System.out.println("\u001B[32mSuccessful Transication\u001B[0m\t");
             Print.receipt(receipt);
         } else {
             selectedSeats = Select.seats(scanObj, hall);
             Select.unSelectSeats(scanObj, selectedSeats, hall);
-            if (confirmMsg(scanObj, movie, selectedSeats, screenTime)) {
+            if (Check.confirmMsg(scanObj, movie, selectedSeats, screenTime)) {
                 var receipt =  customer.bookMovie(screenTime, movie, selectedSeats);
                 System.out.println("\u001B[32mSuccessful Transication\u001B[0m\t");
                 Print.receipt(receipt);
@@ -71,34 +70,6 @@ public class Menu {
         }
     }
 
-    public static boolean confirmMsg (Scanner scanObj, Movie selectedMovie, List<Seat> seats, ScreenTime screenTime) {
-        if (!seats.isEmpty()) {
-            float totalPrice = Order.getPrice(seats);
-            var response = Check.enResponse.UNKNOWN;
-
-            do {
-                System.out.println("Price all is " + totalPrice);
-                System.out.println("Movie Title: " + selectedMovie.getTitle());
-                System.out.println("Hall ID: " + screenTime.getHall().getID());
-                System.out.print("Screen Time: ");
-                Print.screenTime(screenTime);
-
-                System.out.println("Seats: ");
-                for (var seat : seats) {
-                    System.out.print(seat.getID() + ", ");
-                }
-                System.out.println();
-
-                System.out.println("Confirm Payment? (y/n)");
-                String answer = scanObj.next();
-                response = Check.getResponse(answer);
-
-            } while(response == Check.enResponse.UNKNOWN);
-
-            return response == Check.enResponse.YES;
-        }
-        return false;
-    }
 
     public static void generateReports (Scanner scanObj) {
         final String[] reports = {
@@ -120,12 +91,11 @@ public class Menu {
 
         int choice = Select.report(scanObj, reports);
         choice--;
-        System.out.print(reports[choice]);
 
         switch (choice) {
             case 0:
                 Movie movie = Select.movie(scanObj);
-                System.out.println(" on movie \"" + movie.getTitle() + "\" is " + Report.numberOfSoldSeats(movie));
+                System.out.println(reports[choice] + " on movie \"" + movie.getTitle() + "\" is " + Report.numberOfSoldSeats(movie));
                 break;
             case 1:
                 System.out.println();
@@ -148,14 +118,8 @@ public class Menu {
 
 
     public static void listReceipts (Scanner scanObj, Customer customer) {
-        //ps: receipts better to be array for performance
         int choice;
         var receipts = customer.getReceipts();
-
-        /*
-         * 1: Recipet #12
-         * 2: Reciept
-         * */
 
         if (receipts != null && !receipts.isEmpty()) {
             do {
@@ -173,7 +137,45 @@ public class Menu {
     }
 
     public static void addMovie (Scanner scanObj) {
-        String title = scanObj.next();
-        float duration = scanObj.nextFloat();
+        //todo: Implement Adding Movie code
+    }
+
+    public static void deleteMovie (Scanner scanObj) {
+       boolean isEmpty = MovieLibrary.getMovies().isEmpty();
+       if (!isEmpty) {
+           var response = Check.enResponse.YES;
+           do {
+               System.out.println("Do you want to delete movie (y/n)");
+               response = Check.getResponse(scanObj.next());
+
+               if (response == Check.enResponse.UNKNOWN) {
+                   System.out.println("Invalid option. Please try again.");
+                   continue;
+               }
+
+               if (response == Check.enResponse.NO) {
+                   break;
+               }
+
+               var movie = Select.movie(scanObj);
+
+               System.out.println("Are you sure you want delete "+ movie.getTitle() + " movie ?"+" Y/N" );
+               response = Check.getResponse(scanObj.next());
+
+               switch (response) {
+                   case YES:
+                       if (MovieLibrary.deleteMovie(movie)) {
+                           System.out.println(movie.getTitle() + " has been deleted Successfully.");
+                           System.out.println("\u001B[32mMovie \"" + movie.getTitle() +"\" Deleted Successfully\u001B[0m\t");
+                       } else {
+                           System.out.println("\u001B[32mSomething Went Wrong Cannot Delete This Movie\u001B[0m\t");
+                       }
+                       break;
+                   case NO:
+                       System.out.println("Error: Deletion canceled.");
+                        break;
+               }
+           } while (response == Check.enResponse.YES && !MovieLibrary.getMovies().isEmpty());
+       }
     }
 }
