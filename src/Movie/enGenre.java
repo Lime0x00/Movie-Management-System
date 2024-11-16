93fe92c4997aaa6def7c9ef528afd429373b5adb
Ali Magdy <166527591+alimagdye@users.noreply.github.com>
2024-11-16 23:46:11 +0200

    Update enGenre.java
    add documentation

--- src/Movie/enGenre.java
+++ src/Movie/enGenre.java
@@ -1,17 +1,39 @@
 package Movie;
 
+/**
+ * The enGenre enum represents the different genres of movies available in the system.
+ * It includes genres such as Action, Romantic, Horror, and Comedy.
+ */
 public enum enGenre {
+    /** Represents an Action genre movie. */
     ACTION("Action"),
+
+    /** Represents a Romantic genre movie. */
     ROMANTIC("Romantic"),
+
+    /** Represents a Horror genre movie. */
     HORROR("Horror"),
+
+    /** Represents a Comedy genre movie. */
     COMEDY("Comedy");
-    
+
+    /** The description of the genre. */
     private final String genre;
 
+    /**
+     * Constructs an enGenre instance with the specified genre description.
+     *
+     * @param genre the genre description to set.
+     */
     enGenre(String genre){
         this.genre = genre;
     }
-    
+
+    /**
+     * Retrieves the genre description.
+     *
+     * @return the genre description as a string.
+     */
     public String getGenre(){
         return genre;
     }
