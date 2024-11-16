43c0ffb8c83f6972b5b9aa33e250af61c4d46c85
lime0x001 <mohammada4raf@gmail.com>
2024-11-16 03:02:19 +0200

    Refactor Check class to improve input validation and payment confirmation flow
    - Combined response handling into a single `getResponse` method to normalize user input.
    - Added generic `isValidAnswer` method to check valid choices for both arrays and lists.
    - Enhanced `confirmMsg` method to display clearer movie, seat, and price details before payment confirmation.
    - Streamlined confirmation process with clear prompts for "y/n" responses and robust input validation.

--- src/CLI/Check.java
+++ src/CLI/Check.java
@@ -1,7 +1,13 @@
 package CLI;
 
+import Book.Order;
+import Hall.Seat;
+import Movie.Movie;
+import Movie.ScreenTime;
+
 import java.util.List;
 import java.util.Locale;
+import java.util.Scanner;
 
 public class Check {
     public enum enResponse {
@@ -25,6 +31,35 @@ public class Check {
             case "n", "no" -> enResponse.NO;
             default -> enResponse.UNKNOWN;
         };
+    }
+
+    public static boolean confirmMsg (Scanner scanObj, Movie selectedMovie, List<Seat> seats, ScreenTime screenTime) {
+        if (!seats.isEmpty()) {
+            float totalPrice = Order.getPrice(seats);
+            var response = Check.enResponse.UNKNOWN;
+
+            do {
+                System.out.println("Price all is " + totalPrice);
+                System.out.println("Movie Title: " + selectedMovie.getTitle());
+                System.out.println("Hall ID: " + screenTime.getHall().getID());
+                System.out.print("Screen Time: ");
+                Print.screenTime(screenTime);
+
+                System.out.println("Seats: ");
+                for (var seat : seats) {
+                    System.out.print(seat.getID() + ", ");
+                }
+                System.out.println();
+
+                System.out.println("Confirm Payment? (y/n)");
+                String answer = scanObj.next();
+                response = Check.getResponse(answer);
+
+            } while(response == Check.enResponse.UNKNOWN);
+
+            return response == Check.enResponse.YES;
+        }
+        return false;
     }
 
 }
