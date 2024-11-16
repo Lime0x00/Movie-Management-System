da425207deb7463134f31f9b73c6e1de4bf880a4
Amr khaled mahanna <amrkh20002@gmail.com>
2024-11-16 23:43:32 +0200

    Manager.java

--- src/CLI/Manager.java
+++ src/CLI/Manager.java
@@ -1,3 +1,6 @@
+/**
+ * Manages the initialization of resources and the main workflow for the movie booking system.
+ */
 package CLI;
 
 import Book.Customer;
@@ -7,59 +10,76 @@ import Movie.ScreenTime;
 import Movie.MovieLibrary;
 import Movie.enGenre;
 
-import java.lang.String;
 import java.util.Scanner;
 import java.util.Date;
 import java.util.ArrayList;
 
 public class Manager {
     //!TODO
-    //todo: Add Packages
-    //todo: Move class Receipt to class Order (Composition)
-    //todo: Move class seats to class hall (Composition)
-    //todo: Make sure not including all the library for one class
-    //todo: Remove confirmMsg() from Order (Check it here)
-    //todo: Make getPrice() In Order public (In Case Confirm Message here not in Order)
+    // TODO: Add Packages
+    // TODO: Move class Receipt to class Order (Composition)
+    // TODO: Move class seats to class hall (Composition)
+    // TODO: Ensure not including all the library for one class
+    // TODO: Remove confirmMsg() from Order (Check it here)
+    // TODO: Make getPrice() in Order public (In case confirmMsg() is moved here)
 
+    /**
+     * Static list of available halls in the system.
+     */
     static public ArrayList<Hall> halls = new ArrayList<Hall>();
 
-
-    public Manager (Scanner scanObj) {
+    /**
+     * Constructs a Manager instance, initializes the movie library, and starts the workflow.
+     *
+     * @param scanObj the {@link Scanner} object for user input
+     */
+    public Manager(Scanner scanObj) {
         initializeLibrary();
         start(scanObj);
     }
 
-    private void initializeLibrary () {
-        var hall1 = new Hall((byte) 1, 2, 2);  // 10 * 10 = 100 Seat
-        var hall2 = new Hall((byte) 1, 5, 5);  // 10 * 10 = 100 Seat
+    /**
+     * Initializes the library of movies, halls, and their associated screen times.
+     * Sets up test data for demonstration purposes.
+     */
+    private void initializeLibrary() {
+        var hall1 = new Hall((byte) 1, 2, 2);  // Hall with 2x2 = 4 seats
+        var hall2 = new Hall((byte) 1, 5, 5);  // Hall with 5x5 = 25 seats
         var screenTimes1 = new ArrayList<ScreenTime>();
         var screenTimes2 = new ArrayList<ScreenTime>();
+
         halls.add(hall1);
         halls.add(hall2);
-        screenTimes1.add(new ScreenTime(hall1, new Date(2024 - 1900, 10, 21, 12, 20, 30), new Date(2024 - 1900, 10, 21, 14, 20, 30)));
 
+        screenTimes1.add(new ScreenTime(hall1, new Date(2024 - 1900, 10, 21, 12, 20, 30), new Date(2024 - 1900, 10, 21, 14, 20, 30)));
         screenTimes2.add(new ScreenTime(hall1, new Date(2024 - 1900, 11, 21, 1, 0, 0), new Date(2024 - 1900, 11, 21, 2, 0, 0)));
         screenTimes2.add(new ScreenTime(hall2, new Date(2024 - 1900, 11, 22, 1, 0, 0), new Date(2024 - 1900, 11, 22, 2, 0, 0)));
 
         Movie movie = new Movie("Movie1", enGenre.ROMANTIC, screenTimes1);
-        Movie movie1 = new Movie("Movie2" , enGenre.COMEDY, screenTimes2);
+        Movie movie1 = new Movie("Movie2", enGenre.COMEDY, screenTimes2);
+
         var movies = new ArrayList<Movie>();
         movies.add(movie);
         movies.add(movie1);
+
         MovieLibrary lib = new MovieLibrary(movies);
     }
 
-
-    public void start (Scanner scanObj) {
+    /**
+     * Starts the system's main workflow, collecting user input to create a new customer and displaying the main menu.
+     *
+     * @param scanObj the {@link Scanner} object for user input
+     */
+    public void start(Scanner scanObj) {
         System.out.print("Enter your name: ");
         String name = scanObj.next();
 
-        java.lang.System.out.print("Enter your age: ");
+        System.out.print("Enter your age: ");
         short age = scanObj.nextShort();
 
         var customer = new Customer(name, age);
 
-        java.lang.System.out.println("Welcome " + customer.getName());
+        System.out.println("Welcome " + customer.getName());
 
         Menu.mainMenu(scanObj, customer);
     }
